a53b4b098417d9298373d1b70cb3bf912323262f
Itai Levin <itail@mit.edu>
2025-01-29 12:58:42 -0800

    Fixed argument passing for spectral prediction

--- 02_spectral_prediction/predict_optical_properties.sh
+++ 02_spectral_prediction/predict_optical_properties.sh
@@ -1,15 +1,18 @@
 #!bin/bash
 
-while getopts i:n:s:o:m:c: option
+# Runs the pipeline from SMILES through the TD-DFT simulation for one molecule.
+
+while getopts i:n:s:o:m:c:f: option
 	do
 		case "${option}"
 			in
 					i) INPUT_DIR=${OPTARG};;
 					n) NAME=${OPTARG};;
-          				s) SMILES=${OPTARG};;
+          s) SMILES=${OPTARG};;
 					o) OUTPUT_DIR=${OPTARG};;
 					m) MEM=${OPTARG};;
 					c) CPUS=${OPTARG};;
+					f) FUNCTIONAL=${OPTARG};;
 		esac
 done
 
@@ -19,15 +22,13 @@ python biospectral/make_structure_single.py --smiles $SMILES --name $NAME --out-
 echo "...done getting 3D structure from SMILES"
 
 echo "Refining 3D structure and making Gaussian input file..."
-bash biospectral/make_gaus_inputs_single.sh --name $NAME -i $INPUT_DIR -o $INPUT_DIR --gau-output-dir $TMPDIR --mem 40 --cpus 10
+bash biospectral/make_gaus_inputs_single.sh --name $NAME -i $INPUT_DIR -o $INPUT_DIR --gau-output-dir $TMPDIR --mem $MEM --cpus $CPUS
 echo "...done refining 3D structure and making Gaussian input file"
 
 echo "Redefining Gaussian input file for supercloud..."
-bash biospectral/make_diff_functional_inputs_single.sh --name $NAME -i $INPUT_DIR -o $INPUT_DIR --gau-output-dir "/home/gridsan/itail/scratch/gaussian/" --mem $MEM --cpus $CPUS -f "b3lyp"
+bash biospectral/make_diff_functional_inputs_single.sh --name $NAME -i $INPUT_DIR -o $INPUT_DIR --gau-output-dir "/home/gridsan/itail/scratch/gaussian/" --mem $MEM --cpus $CPUS -f $FUNCTIONAL
 echo "...done redefining Gaussian input file for supercloud"
 
 echo "Running TD-DFT simulation..."
-bash biospectral/run_script_single.sh -i $INPUT_DIR -n $NAME"_b3lyp" -o $OUTPUT_DIR
+bash biospectral/run_script_single.sh -i $INPUT_DIR -n $NAME"_$FUNCTIONAL" -o $OUTPUT_DIR
 echo "...done running TD-DFT simulation"
-
-
